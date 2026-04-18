1aa18a4f624f8f41001e8bd5d0224c2bbc81533c
Damian Gnieciak <szkola_abc@o2.pl>
2026-04-18 12:03:32 +0200

    Add docker compose

--- backend/src/BetterTracker/Program.cs
+++ backend/src/BetterTracker/Program.cs
@@ -1,6 +1,7 @@
 using BetterTracker.Api;
 using BetterTracker.Common;
 using BetterTracker.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,12 @@ builder.Services.AddRepositories();
 builder.AddApi();
 
 var app = builder.Build();
+
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
 
 app.UseApi();
 app.MapApi();
